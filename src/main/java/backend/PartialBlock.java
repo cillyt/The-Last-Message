e7e7c71cb5fcd73d8ccd45c4715c3fb0691a4fcf
Piatachenko Hlib <h.piatachenko@ukma.edu.ua>
2026-06-16 04:09:25 +0300

    fix: fix PartBlock

--- src/main/java/backend/PartialBlock.java
+++ src/main/java/backend/PartialBlock.java
@@ -23,43 +23,5 @@ public class PartialBlock extends GameEntity {
         isWalkable = false;
         this.zIndex = 1;
         this.blockDirection = blockDirection;
-
-        Canvas canvas = new Canvas(width, height);
-        GraphicsContext tempGc = canvas.getGraphicsContext2D();
-
-        double startX = 0, startY = 0, endX = 0, endY = 0;
-
-        switch (blockDirection) {
-            case TOP -> { startY = 0; endY = 1; startX = 0; endX = 0; }
-            case BOTTOM -> { startY = 1; endY = 0; startX = 0; endX = 0; }
-            case LEFT -> { startX = 0; endX = 1; startY = 0; endY = 0; }
-            case RIGHT -> { startX = 1; endX = 0; startY = 0; endY = 0; }
-        }
-
-        Color baseColor = Color.web("#6b7785");
-        Color fadeColor = Color.web("#2a2e33", 0.15);
-
-        LinearGradient gradient = new LinearGradient(
-                startX, startY, endX, endY, true, CycleMethod.NO_CYCLE,
-                new Stop(0.0, baseColor),
-                new Stop(1.0, fadeColor)
-        );
-
-        tempGc.setFill(gradient);
-        tempGc.fillRect(0, 0, width, height);
-
-        tempGc.setStroke(Color.web("#2a2e33"));
-        tempGc.setLineWidth(4);
-
-        switch (blockDirection) {
-            case TOP -> tempGc.strokeLine(0, 2, width, 2);
-            case BOTTOM -> tempGc.strokeLine(0, height - 2, width, height - 2);
-            case LEFT -> tempGc.strokeLine(2, 0, 2, height);
-            case RIGHT -> tempGc.strokeLine(width - 2, 0, width - 2, height);
-        }
-
-        SnapshotParameters params = new SnapshotParameters();
-        params.setFill(Color.TRANSPARENT);
-        this.image = canvas.snapshot(params, null);
     }
 }
